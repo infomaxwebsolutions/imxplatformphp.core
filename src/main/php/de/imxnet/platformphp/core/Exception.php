714d860b33a79828a5930a4a0c885cda62f5f71d
Bastian Schwarz <bastian@codename-php.de>
2016-05-16 10:26:30 +0200

    Added base exception by renaming the dummy file

--- src/main/php/de/imxnet/platformphp/core/Exception.php
+++ src/main/php/de/imxnet/platformphp/core/Exception.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2014 Bastian Schwarz <bastian@codename-php.de>.
+ * Copyright 2015 Bastian Schwarz <bastian@codename-php.de>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,16 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * @namespace
  */
-
 namespace de\imxnet\platformphp\core;
 
 /**
+ * Base exception from which all other exceptions should extend.
  *
  * @author Bastian Schwarz <bastian@codename-php.de>
  */
-class TestCase extends \de\codenamephp\platform\test\TestCase {
+class Exception extends \Exception {
 //put your code here
 }
